e3b41deb19e6503d3c06f7afb28ba3ff47b4d2bd
RyanKoech <sirryankoech@gmail.com>
2022-03-10 14:25:46 +0300

    Added jfoenix, sql, fontawsomefx dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module com.example.vigilantdoodle {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires javafx.graphics;
+    requires jfoenix;
+    requires java.sql;
+    requires fontawesomefx;
 
     opens com.example.vigilantdoodle to javafx.fxml;
     exports com.example.vigilantdoodle;

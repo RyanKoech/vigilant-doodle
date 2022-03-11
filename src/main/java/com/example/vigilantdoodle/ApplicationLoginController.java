4d2c3354d54fd6540f1006eea85732c93e1505a1
RyanKoech <sirryankoech@gmail.com>
2022-03-11 12:16:07 +0300

    Updated controller with elements and function skeleton

--- src/main/java/com/example/vigilantdoodle/ApplicationLoginController.java
+++ src/main/java/com/example/vigilantdoodle/ApplicationLoginController.java
@@ -1,14 +1,41 @@
 package com.example.vigilantdoodle;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class ApplicationLoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private JFXButton adminButton;
+
+    @FXML
+    private JFXButton loginButton;
+
+    @FXML
+    private TextField usernameTextfield;
+
+    @FXML
+    private PasswordField passwordTextfield;
+
+    @FXML
+    private JFXButton investigatingTextField;
+
+    @FXML
+    void loginUser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setInvestigatingLoginState(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toggleAdminLoginState(ActionEvent event) {
+
     }
 }

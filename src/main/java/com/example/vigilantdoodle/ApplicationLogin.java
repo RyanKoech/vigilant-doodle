8754bd8b02ab601978d413eb5b97c56e2f56540f
RyanKoech <sirryankoech@gmail.com>
2022-03-11 11:28:33 +0300

    Modified Scene code

--- src/main/java/com/example/vigilantdoodle/ApplicationLogin.java
+++ src/main/java/com/example/vigilantdoodle/ApplicationLogin.java
@@ -11,7 +11,7 @@ public class ApplicationLogin extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ApplicationLogin.class.getResource("application-login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

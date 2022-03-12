f0520d333a6c14434184b73f11fee8b2d4f2bb85
RyanKoech <sirryankoech@gmail.com>
2022-03-12 20:57:32 +0300

    Added database connection code

--- src/main/java/com/example/vigilantdoodle/utilities/MysqlConnector.java
+++ src/main/java/com/example/vigilantdoodle/utilities/MysqlConnector.java
@@ -1,4 +1,22 @@
 package com.example.vigilantdoodle.utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class MysqlConnector {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/casemanagementsystem";
+    static final String USER = "root";
+    static final String PASS = "";
+    public static Connection connectDB(){
+        Connection conn = null;
+        try{
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            return conn;
+        }catch(Exception ex){
+
+            System.out.print("There was an error connecting to the database!");
+            return null;
+        }
+    }
 }

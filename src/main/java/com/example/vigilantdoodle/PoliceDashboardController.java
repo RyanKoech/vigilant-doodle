e534ffb5252e4c59671eda0d7bfe442be2550c58
RyanKoech <sirryankoech@gmail.com>
2022-03-24 20:25:14 +0300

    Added Dashboard and Welcome Banner Logic

--- src/main/java/com/example/vigilantdoodle/PoliceDashboardController.java
+++ src/main/java/com/example/vigilantdoodle/PoliceDashboardController.java
@@ -1,25 +1,35 @@
 package com.example.vigilantdoodle;
 
+import com.example.vigilantdoodle.datamodels.PoliceReports;
+import com.example.vigilantdoodle.utilities.Data;
+import com.example.vigilantdoodle.utilities.MysqlConnector;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
 import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class PoliceDashboardController {
+public class PoliceDashboardController implements Initializable {
 
     @FXML
     private JFXButton dashboardMenuButton;
@@ -40,28 +50,28 @@ public class PoliceDashboardController {
     private VBox dashboardTabVBox;
 
     @FXML
-    private JFXTreeTableView<?> dashboardTableView;
+    private JFXTreeTableView<PoliceReports> dashboardTableView;
 
     @FXML
-    private TreeTableColumn<?, ?> obIdTableColumn;
+    private TreeTableColumn<PoliceReports, String> obIdTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> reporterNameTableColumn;
+    private TreeTableColumn<PoliceReports, String> reporterNameTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> offenderNameTableColumn;
+    private TreeTableColumn<PoliceReports, String> offenderNameTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> locationTableColumn;
+    private TreeTableColumn<PoliceReports, String> locationTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> dateTableColumn;
+    private TreeTableColumn<PoliceReports, String> dateTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> timeTableColumn;
+    private TreeTableColumn<PoliceReports, String> timeTableColumn;
 
     @FXML
-    private TreeTableColumn<?, ?> crimeTypeTableColumn;
+    private TreeTableColumn<PoliceReports, String> crimeTypeTableColumn;
 
     @FXML
     private VBox custodiesTabVBox;
@@ -117,9 +127,15 @@ public class PoliceDashboardController {
     @FXML
     private JFXButton reportButton;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        setWelcomeBannerLabel();
+        showPoliceReports();
+    }
+
     //Logout Button Function
     @FXML
-    void onLogout(ActionEvent event) {
+    private void onLogout(ActionEvent event) {
         try{
             Parent menuParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("application-login.fxml")));
             Scene menuScene = new Scene(menuParent);
@@ -135,30 +151,100 @@ public class PoliceDashboardController {
     //Side Menu Navigation Button Actions
     //No 1
     @FXML
-    void onShowCustodiesTab(ActionEvent event) {
+    private void onShowCustodiesTab(ActionEvent event) {
         custodiesTabVBox.toFront();
     }
 
     //No 2
     @FXML
-    void onShowDashboardTab(ActionEvent event) {
+    private void onShowDashboardTab(ActionEvent event) {
         dashboardTabVBox.toFront();
     }
 
     //No 3
     @FXML
-    void onShowReportingTab(ActionEvent event) {
+    private void onShowReportingTab(ActionEvent event) {
         reportingTabVBox.toFront();
     }
 
     @FXML
-    void onUpdateCustody(ActionEvent event) {
+    private void onUpdateCustody(ActionEvent event) {
 
     }
 
     @FXML
-    void onSearchCase(ActionEvent event) {
+    private void onSearchCase(ActionEvent event) {
 
+    }
+
+    @FXML
+    private void onReportCrime(ActionEvent event){
+
+    }
+
+    //Sets the logged in police name on the welcome banner
+    private void setWelcomeBannerLabel(){
+        Connection dbConn = MysqlConnector.connectDB();
+        if(dbConn != null){
+            try {
+
+                PreparedStatement st = (PreparedStatement)dbConn.prepareStatement ("SELECT Name FROM `users` WHERE `Police_Id` = ?");
+                st.setString(1, Data.POLICE_ID);
+                ResultSet res = st.executeQuery();
+
+                while (res.next()){
+                    String welcomeName = res.getString("Name");
+                    welcomeBannerLabel.setText("Inspector "+ welcomeName);
+
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(PoliceDashboardController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else{
+            System.out.println("The connection is not available");
+        }
+    }
+
+    private ObservableList<PoliceReports> getPoliceReportList(){
+        ObservableList<PoliceReports> policeReportsList = FXCollections.observableArrayList();
+
+        Connection connection = MysqlConnector.connectDB();
+//        String query = "SELECT `OB_id`, `Reporter_Id`, `Offender_Id`, `Location`, `Date`, `Time`, `Crime_Type` FROM `cases` WHERE `Police_Id`=?";
+//        Statement st;
+//        ResultSet rs;
+
+        try {
+//            st = connection.createStatement();
+            PreparedStatement st = (PreparedStatement)connection.prepareStatement ("SELECT `OB_id`, `Reporter_Id`, `Offender_Id`, `Location`, `Date`, `Time`, `Crime_Type` FROM `cases` WHERE `Police_Id`=?");
+            st.setString(1, Data.POLICE_ID);
+            ResultSet res =st.executeQuery();
+            PoliceReports policeReports;
+
+            while(res.next()){
+                policeReports = new PoliceReports(res.getString("OB_id"), res.getString("Reporter_Id"), res.getString("Offender_Id") , res.getString("Location")  , res.getString("Date") , res.getString("Time") , res.getString("Crime_Type"));
+                policeReportsList.add(policeReports);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return policeReportsList;
+    }
+
+    private void showPoliceReports()  {
+        ObservableList<PoliceReports> list = getPoliceReportList();
+
+        obIdTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("obId"));
+        reporterNameTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("reporterName"));
+        offenderNameTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("offenderName"));
+        locationTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("location"));
+        dateTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("date"));
+        timeTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("time"));
+        crimeTypeTableColumn.setCellValueFactory(new TreeItemPropertyValueFactory<PoliceReports, String>("crime"));
+
+        TreeItem<PoliceReports> root = new RecursiveTreeItem<>(list, RecursiveTreeObject::getChildren);
+        dashboardTableView.setRoot(root);
+        dashboardTableView.setShowRoot(false);
     }
 
 }

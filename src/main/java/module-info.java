17de884b265252fba3efb0f6d0f5f0b0b0b9b6ad
RyanKoech <sirryankoech@gmail.com>
2022-03-24 20:24:43 +0300

    Modules access updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,5 +9,8 @@ module com.example.vigilantdoodle {
     requires java.sql;
 
     opens com.example.vigilantdoodle to javafx.fxml;
+    opens com.example.vigilantdoodle.datamodels to javafx.fxml;
+
+    exports com.example.vigilantdoodle.datamodels;
     exports com.example.vigilantdoodle;
 }

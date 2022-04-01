ce436ab4e4d5600323120478d1af1b6e85dc17aa
RyanKoech <sirryankoech@gmail.com>
2022-04-01 20:39:34 +0300

    Added corresponding varaibles in the controller

--- src/main/java/com/example/vigilantdoodle/PoliceAdminDashboardController.java
+++ src/main/java/com/example/vigilantdoodle/PoliceAdminDashboardController.java
@@ -1,4 +1,219 @@
 package com.example.vigilantdoodle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.VBox;
+
 public class PoliceAdminDashboardController {
+
+    @FXML
+    private JFXButton dashboardMenuButton;
+
+    @FXML
+    private JFXButton reportingMenuButton;
+
+    @FXML
+    private JFXButton custodiesMenuButton;
+
+    @FXML
+    private JFXButton addPoliceMenuButton;
+
+    @FXML
+    private JFXButton editPoliceMenuButton;
+
+    @FXML
+    private Label welcomeBannerLabel;
+
+    @FXML
+    private JFXButton logoutButton;
+
+    @FXML
+    private VBox reportingTabVBox;
+
+    @FXML
+    private TextField reporterNameTextField;
+
+    @FXML
+    private TextField reporterIdTextField;
+
+    @FXML
+    private TextField offenderIdTextField;
+
+    @FXML
+    private TextField locationTextField;
+
+    @FXML
+    private TextField dateTextField;
+
+    @FXML
+    private TextField timeTextField;
+
+    @FXML
+    private ChoiceBox<?> crimeTypeChoiceBox;
+
+    @FXML
+    private TextArea descriptionTextArea;
+
+    @FXML
+    private JFXButton reportButton;
+
+    @FXML
+    private VBox custodiesTabVBox;
+
+    @FXML
+    private TextField obNumberTextField;
+
+    @FXML
+    private JFXButton searchButton;
+
+    @FXML
+    private Label suspectNameLabel;
+
+    @FXML
+    private Label currentCustodyLabel;
+
+    @FXML
+    private ChoiceBox<?> custodyTypeChoiceBox;
+
+    @FXML
+    private Label bailFeeLabel;
+
+    @FXML
+    private JFXButton updateCustodyButton;
+
+    @FXML
+    private VBox addPoliceTabVBox;
+
+    @FXML
+    private TextField addPolicePoliceIdTextField;
+
+    @FXML
+    private TextField addPolicePoliceNameTextField;
+
+    @FXML
+    private TextField addPolicePhoneNumberTextField;
+
+    @FXML
+    private TextField addPoliceEmailAddressTextField;
+
+    @FXML
+    private ChoiceBox<?> addPolicePoliceRoleChoiceBox;
+
+    @FXML
+    private JFXButton addPoliceButton;
+
+    @FXML
+    private VBox editPoliceTabVBox;
+
+    @FXML
+    private TextField seachPoliceTextField;
+
+    @FXML
+    private JFXButton searchPoliceButton;
+
+    @FXML
+    private TextField editPolicePoliceIdTextField;
+
+    @FXML
+    private TextField editPolicePoliceNameTextField;
+
+    @FXML
+    private TextField editPolicePoliceNumberTextField;
+
+    @FXML
+    private TextField editPoliceEmailAddressTextField;
+
+    @FXML
+    private JFXButton updatePoliceButton;
+
+    @FXML
+    private JFXButton deletePoliceButton;
+
+    @FXML
+    private VBox dashboardTabVBox;
+
+    @FXML
+    private Label fact1Label;
+
+    @FXML
+    private Label stat1Label;
+
+    @FXML
+    private Label fact2Label;
+
+    @FXML
+    private Label stat2Label;
+
+    @FXML
+    private Label fact3Label;
+
+    @FXML
+    private Label stat3Label;
+
+    @FXML
+    private JFXTreeTableView<?> dashboardTableView;
+
+    @FXML
+    private TreeTableColumn<?, ?> obIdTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> policeNameTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> offenderNameTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> locationTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> dateTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> timeTableColumn;
+
+    @FXML
+    private TreeTableColumn<?, ?> crimeTableColumn;
+
+    @FXML
+    void onAddPolice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDeletePolice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onReportCrime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSearchCase(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSearchPolice(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdateCustody(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onUpdatePolice(ActionEvent event) {
+
+    }
+
 }

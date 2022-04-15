99ab8af6c5810f5f6f1f4539b83bff8198412c0a
RyanKoech <sirryankoech@gmail.com>
2022-04-15 07:22:36 +0300

    Updated Connection URL

--- src/main/java/com/example/vigilantdoodle/utilities/MysqlConnector.java
+++ src/main/java/com/example/vigilantdoodle/utilities/MysqlConnector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class MysqlConnector {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/crimes reporting system";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/crimes reporting system?allowMultiQueries=true";
     static final String USER = "root";
     static final String PASS = "";
     public static Connection connectDB(){

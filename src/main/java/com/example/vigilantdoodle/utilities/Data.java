38ab3d58d905ede271e2ae670c64eb89294139ae
RyanKoech <sirryankoech@gmail.com>
2022-04-15 06:27:33 +0300

    Added Necessary Static Properties and Methods

--- src/main/java/com/example/vigilantdoodle/utilities/Data.java
+++ src/main/java/com/example/vigilantdoodle/utilities/Data.java
@@ -1,6 +1,20 @@
 package com.example.vigilantdoodle.utilities;
 
+import java.util.HashMap;
+
 public class Data {
     public static String POLICE_ID = "";
     public static double BASE_BAIL = 5000.0;
+    public static enum emailInfo {
+        RECIPIENT, RECIPIENTNAME, OBID, REPORTDATE, REPORTTIME,
+    }
+
+    public static  String getEmailBody(final HashMap<emailInfo, String> emailInfoMap){
+        return "Dear " + emailInfoMap.get(emailInfo.RECIPIENTNAME) + ",\n\n" +
+                "This is to notify the case you reported on " + emailInfoMap.get(emailInfo.REPORTDATE) + " " + emailInfoMap.get(emailInfo.REPORTTIME) + " hase been recorded and our officers" +
+                "will get into the matter as soon as possible.\n\n" +
+                "Use the OB Number: " + emailInfoMap.get(emailInfo.OBID) + " from now on to report to the case.\n\n" +
+                "Yours protective,\n" +
+                "The Kenya Police Service";
+    }
 }

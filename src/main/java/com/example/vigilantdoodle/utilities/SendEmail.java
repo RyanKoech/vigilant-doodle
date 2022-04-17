478ddca0114a81b4cc2e4b4c1b4efc02f7b61cce
RyanKoech <sirryankoech@gmail.com>
2022-04-17 08:09:29 +0300

    Added Email and Password Imports and Minor Logic Updates

--- src/main/java/com/example/vigilantdoodle/utilities/SendEmail.java
+++ src/main/java/com/example/vigilantdoodle/utilities/SendEmail.java
@@ -1,6 +1,7 @@
 package com.example.vigilantdoodle.utilities;
 
 import com.example.vigilantdoodle.ApplicationLogin;
+import com.example.vigilantdoodle.privates.PrivateData;
 import com.example.vigilantdoodle.utilities.Data.emailInfo;
 
 import javax.mail.Message;
@@ -9,6 +10,7 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -29,9 +31,10 @@ public class SendEmail {
 
         /*
             NOTE YOU NEED TO TURN NO 'Allow less secure apps in the security settings of the email address'
+            Store the information in com.example.vigilantdoodle.privates (Directory Ignored by Git)
         */
-        String email = "address@email.random";
-        String password = "password";
+        String email = PrivateData.EMAIL_ADDRESS;
+        String password = PrivateData.EMAIL_PASSWORD;
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             @Override
@@ -52,10 +55,12 @@ public class SendEmail {
         try {
             InternetAddress recipientAddress = new InternetAddress(emailInfoMap.get(emailInfo.RECIPIENT));
             Message message = new MimeMessage(session);
+            message.addHeader("Content-Type", "text/html; charset=UTF-8");
             message.setFrom(new InternetAddress(sender));
             message.setRecipient(Message.RecipientType.TO, recipientAddress);
-            message.setSubject("Upcoming Court Meeting For Case ID: " + Data.POLICE_ID);
+            message.setSubject(Data.getReporterEmailSubject());
             message.setText(Data.getReporterEmailBody(emailInfoMap));
+            message.setSentDate(new Date());
             return message;
 
         } catch (Exception ex) {

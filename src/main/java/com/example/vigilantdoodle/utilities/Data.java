6a46fdf869be21cccc5cd1b44a91e5cec3de23bd
RyanKoech <sirryankoech@gmail.com>
2022-04-17 08:55:32 +0300

    Added Invistigator Email Body and Subject Template

--- src/main/java/com/example/vigilantdoodle/utilities/Data.java
+++ src/main/java/com/example/vigilantdoodle/utilities/Data.java
@@ -6,7 +6,7 @@ public class Data {
     public static String POLICE_ID = "";
     public static double BASE_BAIL = 5000.0;
     public static enum emailInfo {
-        RECIPIENT, RECIPIENTNAME, OBID, REPORTDATE, REPORTTIME,
+        RECIPIENT, RECIPIENTNAME, OBID, REPORTDATE, REPORTTIME, EMAIL_BODY, EMAIL_SUBJECT, INVESTIGATING_OFFICER
     }
 
     public static  String getReporterEmailBody(final HashMap<emailInfo, String> emailInfoMap){
@@ -20,5 +20,18 @@ public class Data {
 
     public static String getReporterEmailSubject(){
         return "Reported Case Confirmation";
+    }
+
+    public static String getInvestigatorEmailBody(final HashMap<emailInfo, String> emailInfoMap){
+        return "Dear Inspector" + emailInfoMap.get(emailInfo.INVESTIGATING_OFFICER) + ",\n\n" +
+                "This is to notify you have been assigned as the Lead Investigating Officer over the case #" + emailInfoMap.get(emailInfo.OBID) + ". " +
+                "If you are not at the station, kindly report within the next 24 hours to gather the required documents and information to begin carrying out " +
+                "investigations into the case.\n\n" +
+                "Yours protective,\n" +
+                "The Kenya Police Service";
+    }
+
+    public static String getInvestigatorEmailSubject(){
+        return "New Case Investigation Appointment";
     }
 }

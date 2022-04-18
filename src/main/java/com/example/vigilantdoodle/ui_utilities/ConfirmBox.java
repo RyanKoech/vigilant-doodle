52dbc788603522f3c1bcaa184312c06c26264584
RyanKoech <sirryankoech@gmail.com>
2022-04-18 08:26:56 +0300

    Added Logic for a Confirm Box

--- src/main/java/com/example/vigilantdoodle/ui_utilities/ConfirmBox.java
+++ src/main/java/com/example/vigilantdoodle/ui_utilities/ConfirmBox.java
@@ -1,4 +1,48 @@
 package com.example.vigilantdoodle.ui_utilities;
 
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
 public class ConfirmBox {
+
+    static boolean answer;
+
+    public  static boolean displayConfirmBox(String title, String message){
+        Stage window = new Stage();
+
+        //prevents output from other parts of the program
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label1 = new Label();
+        label1.setText(message);
+
+        Button yesButton = new Button("YES");
+        Button noButton = new Button("NO");
+
+        yesButton.setOnAction(e -> {
+            answer = true;
+            window.close();
+        });
+        noButton.setOnAction(e -> {
+            answer = false;
+            window.close();
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label1, yesButton, noButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+        return answer;
+    }
+
 }
+

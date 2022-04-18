a041ae136056347f7d73efb88867b2088fca0725
RyanKoech <sirryankoech@gmail.com>
2022-04-18 08:27:55 +0300

    Added Logic for a Pop Up Alert

--- src/main/java/com/example/vigilantdoodle/ui_utilities/PopUpAlert.java
+++ src/main/java/com/example/vigilantdoodle/ui_utilities/PopUpAlert.java
@@ -1,4 +1,34 @@
 package com.example.vigilantdoodle.ui_utilities;
 
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
 public class PopUpAlert {
+
+    public  static void displayPopUpAlert(String title, String message){
+        Stage window = new Stage();
+
+        //prevents out put from other parts of the progra
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label1 = new Label();
+        label1.setText(message);
+        Button closeButton = new Button("Close window");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label1, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+    }
+
 }
